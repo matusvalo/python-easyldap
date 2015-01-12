6291c0578dbbc5cc88d66e448332e8585aca7f9a
Matus Valo <matus@raspberrypi>
2015-01-12 23:24:21 +0100

    Fix: Use battr instead of attr

--- pyldap/ldapconnection.py
+++ pyldap/ldapconnection.py
@@ -80,7 +80,7 @@ class LdapConnection(object):
         if battrs is not None:
             for battr_name in battrs:
                 mod = LDAPMod.create_binary(0,
-                                            ldap_encode(attr_name),
+                                            ldap_encode(battr_name),
                                             values=ldap_encode(battrs[battr_name]))
                 mods.append(mod)
 
